1097f6c9e42576aa5c76ff67a73917f37b51617e
paul2803-byte <feichtenschlager10@icloud.com>
2022-11-21 13:54:10 +0100

    Exception handling when Server is not running

--- dke_pr/src/main/java/com/dke/app/StorageService.java
+++ dke_pr/src/main/java/com/dke/app/StorageService.java
@@ -1,5 +1,6 @@
 package com.dke.app;
 
+import org.apache.jena.atlas.web.HttpException;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdfconnection.RDFConnection;
 import java.util.List;
@@ -8,13 +9,13 @@ public class StorageService {
 
     private static final String SERVER = "http://localhost:3030/flight_database/";
 
-    public static void storeAircrafts(List<Model> aircrafts) {
+    public static void storeAircrafts(List<Model> aircrafts) throws HttpException {
         // TODO: implement getting the aircraft from the AircraftService.java and storing them in the knowledge graph
         System.out.println(aircrafts.size());
         aircrafts.forEach(StorageService::storeModel);
     }
 
-    public static void storeStates(List<Model> states) {
+    public static void storeStates(List<Model> states) throws HttpException {
         states.forEach(StorageService::storeModel);
     }
 
